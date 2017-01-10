dd4af5264e5d7eab3506f1b4f8c39fb63cfb119d
aultimus <matthew.ault@cocoon.life>
2017-01-10 16:47:30 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+gaas - Goats as a service
+=======================================
+
+A webserver which serves goat pictures
---
+++ gaas/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
+
+var goatDir = "goat-pics"
+
+// Downloads pictures of goats!
+// TODO: Try the flickr API, or at a push:
+// curl -sA "Chrome" -L "https://www.google.com/search?hl=en&tbm=isch&q=goats"
+// and scrape
+
+func getGoats() {
+}
+
+// isDir returns true if the path exists and is a directory.
+func isDir(path string) bool {
+	fi, err := os.Stat(path)
+	return err == nil && fi.IsDir()
+}
+
+func getGoatPath() string {
+	goats, err := ioutil.ReadDir(goatDir)
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+	numGoats := len(goats)
+
+	if numGoats == 0 {
+		panic("critical lack of goats error")
+	}
+	i := rand.Intn(numGoats)
+	return goatDir + "/" + goats[i].Name()
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var path = getGoatPath()
+	fmt.Println("New query, serving", path)
+	http.ServeFile(w, r, path)
+	fmt.Fprintf(w, "Served a goat, path was %s", r.URL.Path[1:])
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if !isDir(goatDir) {
+		getGoats()
+	}
+	http.HandleFunc("/", handler)
+	// TODO: doesn't seem to be serving to remote hosts,
+	// use http.Server instance instead?
+	http.ListenAndServe("0.0.0.0:8080", nil)
+}
